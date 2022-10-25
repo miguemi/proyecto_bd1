068e60adec4cf2b6383182af914f0f294abfbeb2
MiguelM8 <manuelmiguel80@hotmail.com>
2022-10-25 03:07:01 -0600

    Eliminar correo

--- ProyectoWebBD/settings.py
+++ ProyectoWebBD/settings.py
@@ -149,8 +149,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="anuelmiguel8@gmail.com"
-EMAIL_HOST_PASSWORD="gwgysczlpxrmmhuv"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 
 
 #cargar paquete en boostrap
