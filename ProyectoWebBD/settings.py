9533a5ae276052ec4ef7c1c4b44625e60c126286
MiguelM8 <manuelmiguel80@hotmail.com>
2023-06-16 16:27:47 -0600

    permitir cualquier host

--- ProyectoWebBD/settings.py
+++ ProyectoWebBD/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-we@rlvb*9pdf@+7a310#)%_n9hi6b5$4*cbut77-^^slkon)dl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
